f1b4307d73ade114d3fc9f73f0ef6e8b50cb0948
Abhilash Gupta <abhilashgupta8149@gmail.com>
2024-06-14 15:59:04 +0530

    update

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,2 +1,9 @@
-let date = new Date()
-console.log(date.toLocaleTimeString());
+const x = document.querySelector('#firstNumber')
+const y = document.querySelector('#secondNumber')
+const btn = document.querySelector('#btnAdd')
+const result = document.querySelector('#result')
+
+function add(){
+    const sum = parseInt(x.value) + parseInt(y.value)
+    result.innerHTML = "The sum of numbers is : " + sum
+}
