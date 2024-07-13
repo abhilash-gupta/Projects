737c15bfa9b28a9a6e5d05ca1975e92fbc7f883f
Abhilash Gupta <abhilashgupta8149@gmail.com>
2024-07-13 18:54:05 +0530

    update JS 20

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,12 +1,7 @@
-let date = new Date()
-let hour = date.getHours()
-console.log(`Current hour : ${hour}`);
-if(hour <= 12){
-    console.log(`Good morning!`);
-}
-else if(hour <= 16){
-    console.log(`Good afternoon!`);
-}
-else{
-    console.log(`Good evening!`);
-}
+let add = (a, b) => a + b 
+
+console.log(`Result of addition of 2 numbers is ${add(1, 2)}`);
+console.log(`Result of addition of 2 numbers is ${add(5, 4)}`);
+console.log(`Result of addition of 2 numbers is ${add(10, 12)}`);
+console.log(`Result of addition of 2 numbers is ${add(8, 12)}`);
+console.log(`Result of addition of 2 numbers is ${add(5, 6)}`);
